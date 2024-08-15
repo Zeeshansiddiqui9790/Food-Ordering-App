a5618772febe7cfad4a8b253bf506a62adf8f83a
Muhammad Zeeshan Akram <161469971+Zeeshansiddiqui9790@users.noreply.github.com>
2024-08-16 01:23:10 +0500

    Added commit

--- vite-project/src/screens/Home/Home.jsx
+++ vite-project/src/screens/Home/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Navbar from '../../components/Navbar/Navbar'
 import Footer from '../../components/Footer/Footer'
+import Card from '../../components/Card/Card'
+import Carousel from '../../components/Carousel/Carousel'
 
 function Home() {
   return (
     <div>
         <div><Navbar/></div>
+        <div><Carousel/></div>
+        <div><Card/></div>
         <div><Footer/></div>
     </div>
   )
