97af5bb50b0ab53059c0775941c9acd8b01f5eeb
Muhammad Zeeshan Akram <161469971+Zeeshansiddiqui9790@users.noreply.github.com>
2024-08-16 03:47:24 +0500

    backend initialized

--- vite-project/src/components/Carousel/Carousel.jsx
+++ vite-project/src/components/Carousel/Carousel.jsx
@@ -4,13 +4,13 @@ export default function Carousel() {
   return (
     <div>
         
-        <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel">
+        <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel" style={{objectFit: "contain !important"}}>
   <div className="carousel-indicators">
     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
   </div>
-  <div className="carousel-inner" >
+  <div className="carousel-inner" id='carousel' >
      <div class="carousel-caption" style={{zIndex:"10"}}>
     <form className="d-flex">
         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
